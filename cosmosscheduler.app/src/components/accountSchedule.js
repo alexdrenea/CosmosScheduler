08f09fde472bc3e7bdce27af14c0b0668bdd0b24
AlexDrenea <alexdrenea@hotmail.com>
2019-03-25 19:27:28 -0400

    account schedule view/edit and update service

--- cosmosscheduler.app/src/components/accountSchedule.js
+++ cosmosscheduler.app/src/components/accountSchedule.js
@@ -1,73 +1,199 @@
-import React from 'react';
-import '../styles/accountSchedule.css'
+import React from "react";
+import "../styles/accountSchedule.css";
+
+import { Formik, Form, Field, FieldArray, ErrorMessage } from "formik";
 
 class AccountSchedule extends React.Component {
-    constructor(props, context) {
-        super(props, context);
+  constructor(props, context) {
+    super(props, context);
 
-        this.state = {
-            isEditing: false,
-        };
+    this.state = {
+      isEditing: false,
+      account: this.props.account,
+    };
+  }
+
+  _getEmptyDatabase() {
+    return {
+      name: "",
+      collections: [this._getEmptyCollection()]
+    };
+  }
+
+  _getEmptyCollection() {
+    return {
+      name: "",
+      scheduleItems: []
+    };
+  }
+
+  validateRequired(value) {
+    let error;
+    if (!value) {
+      error = "Please enter a value";
     }
+    return error;
+  }
 
-    _renderDatabase (database) {
-        if (this.state.isEditing)
-        {
-            return ( 
-                <div key={database.name}>
-                    database: 
-                    <input vlalue={database.name}/>
-                    {database.collections.map(collection=>{ return this._renderCollection(collection); })}
-                </div>
-            );
-        } else {
-            return ( 
-                <div key={database.name}>
-                    database: {database.name}
-                    {database.collections.map(collection=>{ return this._renderCollection(collection); })}
-                </div>
-            );
+  validateRequestUnit(value) {
+    let error ='';
+    if (!value) {
+      error = `${error}Please enter a value. `;
+    }
+    if (isNaN(value)) {
+      error = `${error}Please enter a numeric value. `;
+    }
+    if (value % 100 !== 0){
+      error = `${error}\nRUs must be set in 100 increments. `;
+    }
+    if (value < 400){
+      error = `${error}\nRUs must greater than 400. `;
+    }
+    return error;
+  }
+
+  submitForm(values) {
+      this.props.saveCallback(values.data).then(
+        (result) =>{
+          if (result) {
+            this.setState({
+              account: values.data,
+              isEditing: false 
+            });
+          } else {
+            alert("failed to save");
+          }
         }
-    }
+      )
+  }
 
-    _renderCollection (collection) {
-        return(
-            <div key={collection.name}>
-                collection: {collection.name}
-                {collection.scheduleItems.map( (schedule) => { return this._renderScheduleItem(schedule); })}
-            </div>   
-        );
-    }
+  _renderAccountEdit(account, isNewAccount) {
+    return (
+      <ul className="schedule-item">
+        <Formik
+          initialValues={{
+            data: account,
+            isNewAccount: isNewAccount
+          }}
+          onSubmit={values => this.submitForm(values)}
+          //validateOnChange= {false}
+          //validateOnBlur = {false}
+          render={formProps => {
+            return (
+              <Form>
+                Account Name: 
+                <Field name="data.accountName" placeholder="Account Name" validate={this.validateRequired}/>
+                <ErrorMessage component="div" className="error" name="data.accountName"/>
+                <div>Account Key: ******</div>
+                {/* todo: is New account - enter key here*/}
+                <FieldArray //Databases
+                  name="data.databases"
+                  render={arrayHelpers => (
+                    <div>
+                      {formProps.values.data.databases.map(
+                        (database, dbIndex) => (
+                          <div key={`db.${dbIndex}`}>
+                            Database: 
+                            <Field name={`data.databases.${dbIndex}.name`} validate={this.validateRequired}/>
+                            <ErrorMessage component="div" className="error" name={`data.databases.${dbIndex}.name`} />
+                            <FieldArray //Collections
+                              name={`data.databases.${dbIndex}.collections`}
+                              render={arrayHelpers => (
+                                <div>
+                                  {formProps.values.data.databases[dbIndex].collections.map((collection, colIndex) => (
+                                    <div key={`col.${colIndex}`}>
+                                      Collection:
+                                      <Field name={`data.databases.${dbIndex}.collections.${colIndex}.name`} validate={this.validateRequired}/>
+                                      <ErrorMessage component="div" className="error" name={`data.databases.${dbIndex}.collections.${colIndex}.name`}/>
+                                      <FieldArray //Schedules
+                                        name={`data.databases.${dbIndex}.collections.${colIndex}.scheduleItems`}
+                                        render={arrayHelpers => (
+                                          <div>
+                                            {formProps.values.data.databases[dbIndex].collections[colIndex].scheduleItems.map(
+                                              (schedule, schedIndex) => (
+                                                <div key={`sched.${schedIndex}`}>
+                                                  Start hour:{" "}
+                                                  <Field name={`data.databases.${dbIndex}.collections.${colIndex}.scheduleItems.${schedIndex}.startHourUTC`}/>
+                                                  Request Units:{" "}
+                                                  <Field name={`data.databases.${dbIndex}.collections.${colIndex}.scheduleItems.${schedIndex}.requestUnits`} validate={this.validateRequestUnit}/>
+                                                  <ErrorMessage component="div" className="error" name={`data.databases.${dbIndex}.collections.${colIndex}.scheduleItems.${schedIndex}.requestUnits`}/>
 
-    _renderScheduleItem (schedule) {
-        return(
-            <div key={schedule.startHourUTC}>
-                {schedule.startHourUTC}: {schedule.requestUnits} RUs
+                                                  <button type="button" onClick={() =>arrayHelpers.remove(schedIndex)}>Remove Schedule</button>
+                                                </div>
+                                              )
+                                            )}
+                                            <button type="button" onClick={() => arrayHelpers.push("")}>Add Schedule</button>
+                                          </div>
+                                        )}
+                                      />
+                                      <button type="button" onClick={() => arrayHelpers.remove(colIndex)}>Remove Collection</button>
+                                    </div>
+                                  ))}
+                                  <button type="button" onClick={() => arrayHelpers.push(this._getEmptyCollection())}>Add Collection</button>
+                                </div>
+                              )}
+                            />
+                            <button type="button" onClick={() => arrayHelpers.remove(dbIndex)}>Remove Database</button>
+                          </div>
+                        )
+                      )}
+                      <button type="button" onClick={() => arrayHelpers.push(this._getEmptyDatabase())}>Add Database</button>
+                    </div>
+                  )}
+                />
+                <button type="submit" /*disabled={formProps.isSubmitting}*/>Save</button>
+                <button onClick={() => this.setState({ isEditing: false })}>Cancel</button>
+              </Form>
+            );
+          }}
+        />
+      </ul>
+    );
+  }
+
+  _renderAccount(account) {
+    return (
+      <ul className="schedule-item">
+        <div>Account Name: {account.accountName}</div>
+        <div>Account Key: ******</div>
+
+        {account.databases.map(database => {
+          return (
+            <div key={database.name}>
+              Database: {database.name}
+              {database.collections.map(collection => {
+                return (
+                  <div key={collection.name}>
+                    Collection: {collection.name}
+                    {collection.scheduleItems.map(schedule => {
+                      return (
+                        <div key={schedule.startHourUTC}>
+                          {schedule.startHourUTC}: {schedule.requestUnits} RUs
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              })}
             </div>
-        );
-    }
+          );
+        })}
 
-    render() {
-        const data = this.props.accountSchedule;
-        return (
-            <ul className="schedule-item">
-                <li>Account Name: {data.accountName}</li>
-                <li> Account Key: ******</li>
-                {data.databases.map(database => { 
-                    return this._renderDatabase(database); 
-                })}
+        <div>
+          <button onClick={() => this.setState({ isEditing: true })}>
+            Edit
+          </button>
+        </div>
+      </ul>
+    );
+  }
 
-                {this.state.isEditing ? 
-                    (<div><button onClick={()=> this.setState({isEditing: false})}>Cancel</button>
-                     <button>Save</button></div>) : 
-                    (<button onClick={()=> this.setState({isEditing: true})}>Edit</button>) 
-                }
-                
-                
-            </ul>
-        );
-    }
-
+  render() {
+    const account = this.state.account;
+    return this.state.isEditing
+      ? this._renderAccountEdit(account, false)
+      : this._renderAccount(account);
+  }
 }
 
 export default AccountSchedule;
--- cosmosscheduler.app/src/components/accountSchedules.js
+++ cosmosscheduler.app/src/components/accountSchedules.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import AccountSchedule from './accountSchedule'
+import axios from 'axios';
 import '../styles/accountSchedule.css';
+
+import {API_URL, API_HEADERS} from '../api/const';
 
 class AccountSchedules extends React.Component {
   constructor(props, context) {
     super(props, context);
 
     this.state = {
-        data: this.props.data,
+        accounts: this.props.data,
         loading: true,
     };
   }
+
+
+  _saveNewAccount(payload) {
+    return axios.post(API_URL,  payload, API_HEADERS).then(
+      (result) => { 
+        console.log(result.data);
+        //this.setState( { data: result.data, loading: false } );
+        return true;
+      },
+      (err) => { 
+        //this.setState( { data: accounts, loading: false } );
+        console.log(err);
+        return false;
+      }
+    );
+  }
+
+  _updateAccount(payload) {
+    return axios.put(API_URL,  payload, API_HEADERS).then(
+      (result) => { 
+        //this.setState( { data: result.data, loading: false } );
+        return true;
+      },
+      (err) => { 
+        console.log(err);
+        return false;
+      }
+    );
+  }
+
 
   _addNewAccount() {
     const newItem = {
@@ -22,24 +55,24 @@ class AccountSchedules extends React.Component {
     let newData = this.state.data;
     newData.push(newItem);
     this.setState({
-      data: newData
+      accounts: newData
     });
   }
 
   componentWillReceiveProps(nextProps){
     this.setState({
-      data: nextProps.data,
+      accounts: nextProps.data,
       loading: nextProps.loading
     });
   }
 
   render() {
-    const schedules = this.state.data;
+    const accounts = this.state.accounts;
     if (this.state.loading) return "";
     return (
         <ul className="schedule-items--container">
-            {schedules.map(account=>{
-              return <AccountSchedule key={account.accountName} accountSchedule={account}/>
+            {accounts.map(account=>{
+              return <AccountSchedule key={account.accountName} account={account} saveCallback={this._updateAccount}/>
             })}
            
             <li><button onClick={()=> this._addNewAccount()}>Add new</button></li>
