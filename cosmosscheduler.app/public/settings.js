df972980b372198a0ce750d6119a8a59328aca47
AlexDrenea <alexdrenea@hotmail.com>
2019-04-04 19:13:43 -0400

    - update settings json to expose headers as well (need that for function key) - update build.yaml to remove updating the config.json -> done in release pipeline - update arm template to set function secretstorage type in files so it can be read from powershell later

--- cosmosscheduler.app/public/settings.js
+++ cosmosscheduler.app/public/settings.js
@@ -1,1 +1,7 @@
 var API_URL = "//localhost:7071/api/schedules";
+var API_HEADERS = {
+  headers: {
+    'Content-Type': 'application/json',
+    'x-functions-key': 'functionkey'
+  }
+};
--- cosmosscheduler.app/src/const.js
+++ cosmosscheduler.app/src/const.js
@@ -1,7 +1,2 @@
-
 export const API_URL = window.API_URL; //use the settings.js file in public to inject settings at build time (https://stackoverflow.com/questions/50369477/create-react-app-configuration-file-after-build-app)
-export const API_HEADERS = {
-    header: {
-      'Content-Type': 'application/json'
-    }
-};
+export const API_HEADERS = window.API_HEADERS;
